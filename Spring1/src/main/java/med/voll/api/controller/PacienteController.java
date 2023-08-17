bf189e707e827c7b1e9f5f35ef09dc910690e1a1
nardino <lucas.diasnardino@gmail.com>
2023-08-17 17:10:49 -0300

    cadastro pacientes sql

--- Spring1/src/main/java/med/voll/api/controller/PacienteController.java
+++ Spring1/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,18 +1,27 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.Paciente;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
     
     @PostMapping
+    @Transactional
     public void cadastrarPaciente(@RequestBody DadosCadastroPaciente payload){
-        System.out.println("Dados recebidos " + payload);
+        repository.save(new Paciente(payload));
     }
 }
