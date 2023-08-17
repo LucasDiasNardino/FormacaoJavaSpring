39754f76630ef5a1c601e8be89ee5074f31d6a9b
nardino <lucas.diasnardino@gmail.com>
2023-08-16 21:21:53 -0300

    aula2.6

--- Spring1/src/main/java/med/voll/api/endereco/Endereco.java
+++ Spring1/src/main/java/med/voll/api/endereco/Endereco.java
@@ -17,4 +17,14 @@ public class Endereco {
     String uf;
     String complemento;
     String numero;
+
+    public Endereco(DadosEndereco dados){
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+    }
 }
