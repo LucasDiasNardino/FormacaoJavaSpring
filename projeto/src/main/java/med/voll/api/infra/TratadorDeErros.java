b184f00cc29ac163ee9eeecf5c1af37c9bf244ee
nardino <lucas.diasnardino@gmail.com>
2023-08-21 20:31:47 -0300

    tratando 400

--- projeto/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ projeto/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -3,6 +3,8 @@ package med.voll.api.infra;
 import javax.swing.text.html.parser.Entity;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,5 +16,20 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception){
+        var erros = exception.getFieldErrors().stream().map(DadosErroValidacao::new).toList(); 
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    private record DadosErroValidacao(
+        String campo,
+        String mensagem){
+
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
