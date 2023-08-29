2bc4782061f66f8a955c21c8db2928d3f89e5a62
nardino <lucas.diasnardino@gmail.com>
2023-08-29 17:05:54 -0300

    recuperando token

--- projeto/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ projeto/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -16,6 +16,18 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {           
         
+        var tokenJWT = recuperarTokenJWT(request);
+        System.out.println(tokenJWT);
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarTokenJWT(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null){
+            throw new RuntimeException("Token JWT não informado");
+        }
+
+        return authHeader;
     }
 }
